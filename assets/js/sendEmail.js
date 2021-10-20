4687c007ae371ae5ff761434a5104b2406d5ecf2
SerjMartin <serjmartin@mail.com>
2021-10-20 16:18:26 +0000

    Add notes to send mail function

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,16 +1,17 @@
+
 function sendMail(contactForm) {
-    emailjs.send("gmail", "rosi", {
+    emailjs.send("gmail", "rosie", {
         "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "project_request": contactForm.projectsummary.value
+        "from_email": contactForm.emailaddress.value, // parametrs from Emailjs
+        "message": contactForm.projectsummary.value
     })
     .then(
         function(response) {
-            console.log("SUCCESS", response);
+            console.log("SUCCESS", response);   //promises
         },
         function(error) {
             console.log("FAILED", error)
         }
     );
-    return false;
+    return false; // To block from loading a new page
 }
